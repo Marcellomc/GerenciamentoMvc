40506a1fc6f30a737397ec0948369923e72383f8
Marcello <mcdescartaveis@hotmail.com>
2022-03-08 21:19:37 -0300

    AtualizBanco

--- Gerenciamento.Dao/Properties/Settings.Designer.cs
+++ Gerenciamento.Dao/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Gerenciamento.Dao.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace Gerenciamento.Dao.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MVSQLSERVER;Initial Catalog=Gerencia_OLTP;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DEVCASA;Initial Catalog=DB_Gerencia;Integrated Security=True")]
         public string banco {
             get {
                 return ((string)(this["banco"]));
